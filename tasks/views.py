adf03720c42a35e0337846d716b6e1171acc1a84
Tomkas <vecchitomas@gmail.com>
2024-02-19 12:42:00 -0300

    Mejorando login
    Registros de usuario
    Manejando errores
    
    Video hasta 36:49

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 # Create your views here.
 
 #Esto crea una "pagina" con contenido hello world
@@ -15,7 +16,31 @@ def funcion(request):
 """
 #Y dentro del HTML, se pone en entre doble llave, {{}}, la variable, que seria show
 
-def helloworld(request):
-    return render(request, 'signup.html', {
-        'form': UserCreationForm
-    })
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse("Usuario creado")
+            except:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'Username already existe'
+                })
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'Password do not match'
+        })
+        # print(request.POST) Esto envia los datos del formulario, super importante
