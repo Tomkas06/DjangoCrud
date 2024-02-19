39c165d1b939a20cf1fb0d82725518fd686c8458
Tomkas <vecchitomas@gmail.com>
2024-02-19 15:56:56 -0300

    Templates y condicionales
    Se continuo con el proyecto y se termino  la parte de templates y condicionales.
    minuto 52:11
    
    Sigue el logout

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     #Se añade la pagina que esta en views
     path('', views.home, name="home"),
     path('signup/', views.signup, name="signup"),
+    path('tasks/', views.tasks, name="tasks"),
+    path('logout/', views.logout, name="logout")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
 # Create your views here.
 
 #Esto crea una "pagina" con contenido hello world
@@ -29,18 +30,24 @@ def signup(request):
         })
     else:
         if request.POST['password1'] == request.POST['password2']:
-
             try:
                 user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return HttpResponse("Usuario creado")
-            except:
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
                 return render(request, 'signup.html', {
                     'form': UserCreationForm,
-                    'error': 'Username already existe'
+                    'error': 'User already exists'
                 })
         return render(request, 'signup.html', {
             'form': UserCreationForm,
             'error': 'Password do not match'
         })
         # print(request.POST) Esto envia los datos del formulario, super importante
+        
+def tasks(request):
+    return render(request, 'tasks.html')
+
+def logout(request):
+    ...
