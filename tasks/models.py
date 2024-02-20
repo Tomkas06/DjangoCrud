224d8416a63bcf591ad04e2e40eb597cafaeb409
Tomkas <vecchitomas@gmail.com>
2024-02-20 12:42:58 -0300

    Modelo de tareas
    Se creo el modelo de las tareas y ademas la creacion del admin, que puede leer todas las tareas.
    Clase siguiente se creara una pagina que visualisara las tareas.
    
    Minuto 1:20:07
    
    Y por ahora no se uso ningun ;

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+#Aqui se crean las tablas de la DB
+#Luego con makemigrations y despues migrate
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    
+    def __str__(self):
+        #Esto muestra el titulo de la tarea
+        return self.title + ' by ' + self.user.username
