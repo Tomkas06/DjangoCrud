2321f3205ac1899f57c06a787bee7a87c0227fe2
Tomkas <vecchitomas@gmail.com>
2024-02-21 12:27:58 -0300

    Mejorando tasks
    Cambios:
    -Cambios en los nombres del url
    -Tasks.html añadiendo que aparezcan la descripcion y la fecha
    
    Añadidos:
    -Botones para actualizar la tarea, eliminar la tarea y ver la tarea
    -El usuario tiene que estar registrado para acceder a las tareas
    
    Sigue boostrap y para publicar la web.

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -24,7 +24,11 @@ urlpatterns = [
     #Se añade la pagina que esta en views
     path('', views.home, name="home"),
     path('tasks/', views.tasks, name="tasks"),
-    path('create_task/', views.create_task, name="create_task"),
+    path('tasks_completed/', views.tasks_completed, name="tasks_completed"),
+    path('tasks/create/', views.create_task, name="create_task"),
+    path('tasks/<int:task_id>/', views.task_detail, name="task_detail"),
+    path('tasks/<int:task_id>/complete/', views.complete_task, name="complete_task"),
+    path('tasks/<int:task_id>/delete/', views.delete_task, name="delete_task"),
     path('signup/', views.signup, name="signup"),
     path('logout/', views.signout, name="logout"),
     path('signin/', views.signin, name="signin"),
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 #Esto crea una "pagina" con contenido hello world
@@ -22,14 +25,55 @@ def funcion(request):
 
 def home(request):
     return render(request, 'home.html')
-      
-def tasks(request):
-    tasks = Task.objects.filter(user=request.user)
-    
-    return render(request, 'tasks.html', {
-        'tasks': tasks
-    })
 
+@login_required  
+def tasks(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'tasks.html', {'tasks': tasks})
+
+@login_required
+def tasks_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, 'tasks.html', {'tasks': tasks})
+
+@login_required
+def task_detail(request, task_id):
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=task_id, user = request.user)
+        form = TaskForm(instance=task)
+        return render(request, 'task_detail.html', {
+            'task': task,
+            'form': form
+        })
+    else:
+        try:
+            task = get_object_or_404(Task, pk=task_id, user = request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'task_detail.html', {
+                'task': task,
+                'form': form,
+                'error': 'Error actualizando la tarea'
+            })
+
+@login_required
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user = request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('tasks')
+
+@login_required
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user = request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
+
+@login_required
 def create_task(request):
    if request.method == 'GET':
         return render(request, 'create_task.html', {
@@ -48,6 +92,7 @@ def create_task(request):
                 'error': 'Pruebe datos validos'
             })
 
+@login_required
 def signout(request):
     logout(request)
     return redirect('home')
